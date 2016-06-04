15743536dba06b5375c084df593c4079b954df0b
Matt McHughes <mmchughes@mtmrecognition.com>
2016-06-04 16:04:15 -0500

    created category model

---
+++ NachosMedia/NachosMedia/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NachosMedia.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
