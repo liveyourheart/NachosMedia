1f5d86ef3fe3833edeec821851bb4bba58a962b0
Matt McHughes <mmchughes@mtmrecognition.com>
2016-06-15 00:22:39 -0500

    added initial properties to library class

--- NachosMedia/NachosMedia/Models/Library.cs
+++ NachosMedia/NachosMedia/Models/Library.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,20 @@ namespace NachosMedia.Models
 {
     public class Library
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string AdminName { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public IEnumerable<MediaRecord> MediaRecords { get; set; }
+
+        
     }
 }
