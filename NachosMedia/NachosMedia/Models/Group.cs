47671c7f32bfea16b82ea56689c872e388684deb
Matt McHughes <mmchughes@mtmrecognition.com>
2016-06-15 00:04:09 -0500

    added initial fields to group model

--- NachosMedia/NachosMedia/Models/Group.cs
+++ NachosMedia/NachosMedia/Models/Group.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,16 @@ namespace NachosMedia.Models
 {
     public class Group
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<ApplicationUser> ListOfMembers { get; set; }
+
+
     }
 }
